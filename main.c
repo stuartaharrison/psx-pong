84192595c9368eeb957edfa2d70c767bafbd2a78
Stuart Harrison <email@stuart-harrison.com>
2018-09-15 18:13:16 +0100

    Updated pong.h for definitions and drawing main items on screen

--- main.c
+++ main.c
@@ -8,6 +8,12 @@
 #include "constants.h"
 #include "pong.h"
 
+Ball ball;
+int batOffset = 20;
+Scoreboard scoreboard;
+Bat batLeft, batRight;
+
+
 int main() {
 	initialize();
 
@@ -16,19 +22,45 @@ int main() {
 		draw();
 		display();
 	}
+	
+	return 0;
 }
+
 
 void initialize() {
 	initializeScreen();
 	initializePad();
 	
+	// define the background colour
 	setBackgroundColor(createColor(50, 50, 50));
+	
+	// define the 'playing field'
+	
+	// define the bat objects
+	batLeft = createBat(batOffset);
+	batRight = createBat(SCREEN_WIDTH - batOffset);
+	
+	// define the ball
+	ball = createBall();
+	
+	// define the scoreboard
 }
+
 
 void update() {
 	padUpdate();
 }
 
-void draw() {
 
+void draw() {
+	// draw the 'playing field'
+
+	// draw the 'bat' objects
+	drawBat(batLeft);
+	drawBat(batRight);
+	
+	// draw the 'ball' object
+	drawBall(ball);
+	
+	// draw the scoreboard
 }
